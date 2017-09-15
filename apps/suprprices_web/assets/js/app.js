155a6f275bbacb1076f2fc9d46eaa89ebfb0f1ac
Aland Kuang <Aland.Kuang@aexp.com>
2017-09-15 15:45:40 -0700

    Beginning a new journey.

--- apps/suprprices_web/assets/js/app.js
+++ apps/suprprices_web/assets/js/app.js
@@ -2,22 +2,9 @@ import React                    from 'react';
 import ReactDOM                 from 'react-dom';
 import { browserHistory }       from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
-
-/*const store = configureStore(browserHistory);
-const history = syncHistoryWithStore(browserHistory, store);
-
-const target = document.getElementById('main_container');
-const node = <Root routerHistory={history} store={store} />;
-
-ReactDOM.render(node, target);*/
-
-class HelloWorld extends React.Component {
-    render() {
-      return (<h1>Hello World!</h1>)
-    }
-  }
+import MainPage                 from './components/mainpage';
   
-  ReactDOM.render(
-    <HelloWorld/>,
+ReactDOM.render(
+    <MainPage/>,
     document.getElementById("hello-world")
-  )
+)
---
+++ apps/suprprices_web/assets/js/components/mainpage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class MainPage extends React.Component {
+    render() {
+      return (<div className="jumbotron">
+                <h1>Suprprices</h1>
+                <p> Find the cheapest grocery around town!</p>
+                
+            </div>)
+    }
+}
+
