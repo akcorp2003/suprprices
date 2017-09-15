2c4da20d5779321381561d5dd333cd2f41342730
Aland Kuang <Aland.Kuang@aexp.com>
2017-09-14 18:10:18 -0700

    Added some controller code. Not going to do auth now.

---
+++ apps/suprprices_web/lib/suprprices_web/controllers/api/v1/groceryitem_controller.ex
@@ -0,0 +1,5 @@
+defmodule SuprpricesWeb.GroceryItemController do
+    # use SuprpricesWeb.Web, :controller
+    use SuprpricesWeb, :controller
+
+end
--- apps/suprprices_web/lib/suprprices_web/router.ex
+++ apps/suprprices_web/lib/suprprices_web/router.ex
@@ -13,6 +13,14 @@ defmodule SuprpricesWeb.Router do
     plug :accepts, ["json"]
   end
 
+  scope "/api", SuprpricesWeb do
+    pipe_through :api
+
+    scope "/v1" do
+      post "/groceryitems", GroceryItemController, :create
+    end
+  end
+
   scope "/", SuprpricesWeb do
     pipe_through :browser # Use the default browser stack
 
