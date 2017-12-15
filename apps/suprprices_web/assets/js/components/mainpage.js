4b30018af0a2d8346c3512e803e9ba6ffbb51b9b
Aland Kuang <Aland.Kuang@aexp.com>
2017-12-15 12:51:46 -0700

    Added an About link

--- apps/suprprices_web/assets/js/components/mainpage.js
+++ apps/suprprices_web/assets/js/components/mainpage.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 export default class MainPage extends React.Component {
     render() {
       return (<div className="jumbotron">
                 <h1>Suprprices</h1>
                 <p> Find the cheapest grocery around town!</p>
-                
+                <Link to="about" className="btn btn-primary btn-lg">About</Link>
             </div>)
     }
 }
+
+//
