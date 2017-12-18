32c4c608ec7b8258c35d422844960bbab459df3e
Aland Kuang <Aland.Kuang@aexp.com>
2017-12-18 09:19:40 -0700

    Removed app.js.
    Not sure about removing the app.js. Recompiled and noticed that nothing was affected. In case it is needed again, this is where you find it.

--- apps/suprprices_web/assets/js/app.js
+++ apps/suprprices_web/assets/js/app.js
@@ -1,22 +1,27 @@
 import 'babel-polyfill'
 import React from 'react';
 import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 //import { BrowserRouter, Switch, Route } from 'react-router';
-import { BrowserRouter, Route, Switch} from 'react-router-dom'
-import CitiesPage from './components/cities/citiesPage'
-import AboutPage from './components/about/aboutpage'
-import MainPage from './components/mainpage'
-import App from './components/app'
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import CitiesPage from './components/cities/citiesPage';
+import AboutPage from './components/about/aboutpage';
+import MainPage from './components/mainpage';
 
-render(
-    <BrowserRouter>
-    <div>
-        <Switch>
-            <Route exact path='/' component={MainPage} />
-            <Route path='/about' component={AboutPage} />
-            <Route path='/cities' component={CitiesPage} />
-        </Switch>
-    </div>
-    </BrowserRouter>,
-    document.getElementById("hello-world")
-);
+class App extends React.Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <div>
+                    <Switch>
+                        <Route exact path='/' component={MainPage} />
+                        <Route path='/about' component={AboutPage} />
+                        <Route path='/cities' component={CitiesPage} />
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("hello-world"));
--- apps/suprprices_web/assets/js/routes.js
+++ apps/suprprices_web/assets/js/routes.js
@@ -3,7 +3,6 @@ import {Route, IndexRoute, Switch} from 'react-router'
 import CitiesPage from './components/cities/citiesPage'
 import AboutPage from './components/about/aboutpage'
 import MainPage from './components/mainpage'
-import App from './components/app'
 
 export default (
     <Switch>
